e4d9dddca5563e0160c7b704510f8052e43b980c
Chidiogo <chidiogo@gmail.com>
2019-08-02 06:39:22 +0100

    create database

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+// $(document).ready(function(){
+
+//         //contact us
+
+//   $('#contactButton').on('click', function(e) {
+//     let data = {
+//       name: $('#userName').val(),
+//       email: $('#userEmail').val(),
+//       subject: $('#userSubject').val(),
+//       message: $('#userMessage').val()
+//     }
+//     createContact(data);
+//     $('#contactForm').trigger('reset');
+//     $('#contactForm').show();
+//     e.preventDefault();
+//     })
+//     function createContact(contactData) {
+//       $.ajax({
+//         url: 'http://localhost:3000/contact',
+//         method: 'POST',
+//         data: contactData,
+//         success: function() {
+//           alert('Message sent successfully, I will get back to you shortly!');
+//           console.log(contactData);
+//         }
+//       });
+//     }
+// })

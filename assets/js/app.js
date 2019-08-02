ff76cc2884a8c7eaf0eb0545a5377695f6876662
Chidiogo <chidiogo@gmail.com>
2019-08-02 06:46:33 +0100

    post to database

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,15 +14,15 @@ $(document).ready(function(){
     $('#contactForm').show();
     e.preventDefault();
     })
-//     function createContact(contactData) {
-//       $.ajax({
-//         url: 'http://localhost:3000/contact',
-//         method: 'POST',
-//         data: contactData,
-//         success: function() {
-//           alert('Message sent successfully, I will get back to you shortly!');
-//           console.log(contactData);
-//         }
-//       });
-//     }
-// })
+    function createContact(contactData) {
+      $.ajax({
+        url: 'http://localhost:3000/contact',
+        method: 'POST',
+        data: contactData,
+        success: function() {
+          alert('Message sent successfully, I will get back to you shortly!');
+          console.log(contactData);
+        }
+      });
+    }
+})
